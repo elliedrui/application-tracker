bba65421971f6f6f16541ff1e09ed3602b22513a
jaesonwatts <jaeson.watts@gmail.com>
2020-12-08 13:28:48 -0700

    user model created sessions/users controllers done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'sessions#home'
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  match '/auth/:provider/callback', to: 'sessions#googlecreate', via: %i[get post], as: 'google'
+  get '/auth/google_oauth2/callback' => 'sessions#googlecreate'
+
+  resources :users
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'money', last_name: 'maker', email: 'moneymakerwats@gmail.com', uid: '', password: '1234' )
+User.create(first_name: 'frank', last_name: 'frizzle', email: 'frank@gmail.com', uid: '', password: '1234' )
+User.create(first_name: 'Jaeson', last_name: 'Watts', email: 'jaeson.watts@gmail.com', uid: '', password: '1234' )
